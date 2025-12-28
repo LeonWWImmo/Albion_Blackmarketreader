e83a6cc41025352b55261854bd28e5982ca675ee
LeonWWImmo <leon.neuhaus@wwimmo.ch>
2025-12-28 11:30:40 +0100

    #0025 Fixing Scripts

--- Albion_ProfitChecker/obj/Debug/net8.0/Albion_ProfitChecker.AssemblyInfo.cs
+++ Albion_ProfitChecker/obj/Debug/net8.0/Albion_ProfitChecker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Albion_ProfitChecker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c78c82266dcfa13cedc3113e6dd5bd855e095863")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+480b3a56e2c5cfa66625f04b18ffb62ac9cf1174")]
 [assembly: System.Reflection.AssemblyProductAttribute("Albion_ProfitChecker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Albion_ProfitChecker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
